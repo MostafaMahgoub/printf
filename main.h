c7c44095feb442469f663b3ade4b71326c69f100
MostafaReda <mostafamahgoub@outlook.com>
2023-07-26 06:32:48 -0700

    Test

--- main.h
+++ main.h
@@ -1,15 +1,49 @@
-#ifndef PRINT_HELPERS_H
-#define PRINT_HELPERS_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _printf(const char *format, ...);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
 int _putchar(char c);
-int _strlen(char *str);
-int print_integer(int n);
-int print_unsigned(unsigned int n, int base, int uppercase);
-int print_special_string(char *str);
-int print_pointer(void *ptr);
-int print_reversed(char *str);
-int print_rot13(char *str);
-int print_binary(unsigned int n);
+int _printf(const char *format, ...);
 
-#endif 
+#endif
